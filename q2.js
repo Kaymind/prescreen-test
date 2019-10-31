ca26addcd6c9b2156bd0f774cbbac28ab3d9ebbc
Kaymind <lempelziv@gmail.com>
2019-10-31 17:28:16 +0700

    code formatting

--- q2.js
+++ q2.js
@@ -6,14 +6,15 @@ const data = [
 
 const processJSON = (data) => {
     // Sort the data first then use the return array to manipulate data
-    const result = data.sort((a, b) => a.name.localeCompare(b.name)).map(item => {
-    const name = item.name.charAt(0).toUpperCase() + item.name.slice(1);
-    const cars = item.cars.join(', ');
-    const fullWord = name + ' want to buy ' + cars;
-    console.log(fullWord);
-    return fullWord;
-    });
-}
+    const result = data.sort((a, b) => a.name.localeCompare(b.name))
+                .map(item => {
+                    const name = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+                    const cars = item.cars.join(', ');
+                    const fullWord = name + ' want to buy ' + cars;
+                    console.log(fullWord);
+                    return fullWord;
+                });
+    }
 
 processJSON(data);
 
